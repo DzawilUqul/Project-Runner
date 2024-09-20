00586bb858788fff3fd72e448ca7701050c1037b
Mochamad Farhan Abdullah <112877057+MochamadFarhanAbdullah@users.noreply.github.com>
2024-09-20 14:15:21 +0700

    refactor: Change obstacle spawn's method

--- Assets/Script/ObstacleSpawner.cs
+++ Assets/Script/ObstacleSpawner.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class ObstacleSpawner : MonoBehaviour
 {
     public GameObject[] obstacles;
-    [SerializeField] private float spawnTime = 2f;
+    [SerializeField] private float spawnTime;
+    [SerializeField] private Transform spawnLocation;
     private float timer;
 
     void Update()
@@ -24,13 +25,14 @@ public class ObstacleSpawner : MonoBehaviour
         GameObject obstacle = Instantiate(obstacles[index]);
 
         // Tentukan posisi x dan z obstacle berdasarkan posisi spawner (misalnya di depan Dino)
-        float spawnX = transform.position.x;
-        float spawnZ = transform.position.z;
+        float spawnX = spawnLocation.transform.position.x;
+        float spawnZ = spawnLocation.transform.position.z;
 
         // Tentukan posisi y sesuai dengan obstacle yang dipilih
         float spawnY = 0f; // Default posisi y
         if (index == 0) spawnY = 0f;   // Obstacle1: y = 1
         else if (index == 1) spawnY = 0f; // Obstacle2: y = 2
+        else if (index == 2) spawnY = 0f;
 
         // Atur posisi obstacle
         obstacle.transform.position = new Vector3(spawnX, spawnY, spawnZ);
